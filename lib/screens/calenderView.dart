9225de4dd6d01a9f5aac204aec7b6f26b6394e08
Ammar Afzal <ammarafzal335@icloud.com>
2021-05-09 01:09:55 +0500

    Calender

---
+++ lib/screens/calenderView.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+import 'package:intl/intl.dart';
+
+class CalenderView extends StatefulWidget {
+  @override
+  _CalenderViewState createState() => _CalenderViewState();
+}
+
+class _CalenderViewState extends State<CalenderView> {
+  DateTime _currentDate = DateTime.now();
+  DateTime _currentDate2 = DateTime.now();
+  String _currentMonth = DateFormat.yMMM().format(DateTime.now());
+  DateTime _targetDateTime = DateTime.now();
+
+  CalendarCarousel _calendarCarouselNoHeader;
+
+  static Widget _eventIcon = new Container(
+    decoration: new BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(1000)),
+        border: Border.all(color: Colors.blue, width: 2.0)),
+    child: new Icon(
+      Icons.person,
+      color: Colors.amber,
+    ),
+  );
+
+  EventList<Event> _markedDateMap = new EventList<Event>(
+    events: {
+      new DateTime(2021, 5, 14): [
+        new Event(
+          date: new DateTime(2021, 5, 14),
+          title: 'Event 1',
+          icon: _eventIcon,
+          dot: Container(
+            margin: EdgeInsets.symmetric(horizontal: 1.0),
+            color: Colors.red,
+            height: 5.0,
+            width: 5.0,
+          ),
+        ),
+      ],
+    },
+  );
+
+  @override
+  void initState() {
+
+    _markedDateMap.addAll(new DateTime(2021, 5, 14), [
+      new Event(
+        date: new DateTime(2021, 5, 14),
+        title: 'Event 1',
+        icon: _eventIcon,
+      ),
+    ]);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _calendarCarouselNoHeader = CalendarCarousel<Event>(
+      todayBorderColor: Colors.green,
+      onDayPressed: (DateTime date, List<Event> events) {
+        this.setState(() => _currentDate2 = date);
+        events.forEach((event) => print(event.title));
+      },
+      daysHaveCircularBorder: true,
+      showOnlyCurrentMonthDate: false,
+      weekendTextStyle: TextStyle(
+        color: Colors.red,
+      ),
+      thisMonthDayBorderColor: Colors.grey,
+      weekFormat: false,
+      height: 420.0,
+      selectedDateTime: _currentDate2,
+      targetDateTime: _targetDateTime,
+      customGridViewPhysics: NeverScrollableScrollPhysics(),
+      markedDateCustomShapeBorder:
+      CircleBorder(side: BorderSide(color: Colors.yellow)),
+      markedDateCustomTextStyle: TextStyle(
+        fontSize: 18,
+        color: Colors.blue,
+      ),
+      showHeader: false,
+      todayTextStyle: TextStyle(
+        color: Colors.blue,
+      ),
+
+      todayButtonColor: Colors.yellow,
+      selectedDayTextStyle: TextStyle(
+        color: Colors.yellow,
+      ),
+      inactiveDaysTextStyle: TextStyle(
+        color: Colors.tealAccent,
+        fontSize: 16,
+      ),
+    );
+
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Calendar'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              //custom icon
+
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30.0,
+                  bottom: 16.0,
+                  left: 16.0,
+                  right: 16.0,
+                ),
+                child: new Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                          _currentMonth,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0,
+                          ),
+                        )),
+                    FlatButton(
+                      child: Text('PREV'),
+                      onPressed: () {
+                        setState(() {
+                          _targetDateTime = DateTime(
+                              _targetDateTime.year, _targetDateTime.month - 1);
+                          _currentMonth =
+                              DateFormat.yMMM().format(_targetDateTime);
+                        });
+                      },
+                    ),
+                    FlatButton(
+                      child: Text('NEXT'),
+                      onPressed: () {
+                        setState(() {
+                          _targetDateTime = DateTime(
+                              _targetDateTime.year, _targetDateTime.month + 1);
+                          _currentMonth =
+                              DateFormat.yMMM().format(_targetDateTime);
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16.0),
+                child: _calendarCarouselNoHeader,
+              ), //
+            ],
+          ),
+        ));
+  }
+}
--- lib/screens/eventsViewer.dart
+++ lib/screens/eventsViewer.dart
@@ -3,6 +3,8 @@ import 'package:nilesisters/localization/demo_localization.dart';
 import 'package:nilesisters/screens/events_api.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import 'calenderView.dart';
 class EventsViewer extends StatefulWidget {
   @override
   _EventsViewerState createState() => _EventsViewerState();
@@ -44,7 +46,7 @@ class _EventsViewerState extends State<EventsViewer> {
                           ListTile(
                             title: Text(DemoLocalization.of(context)
                                 .getTranslatedValue('date'),style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 20),),
-                            trailing: Text('${eventss.dates}',style: TextStyle(fontSize: 20),),
+                            trailing: Text('${eventss.dates.day}' + '-' +'${eventss.dates.month}' + '-' +'${eventss.dates.year}',style: TextStyle(fontSize: 20),),
                           ),
                           ListTile(
                             title: Text(DemoLocalization.of(context)
@@ -67,23 +69,19 @@ class _EventsViewerState extends State<EventsViewer> {
                                 .getTranslatedValue('benefits'),style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 20),),
                             trailing: Text(' ${eventss.benifits}',style: TextStyle(fontSize: 20),),
                           ),
-                          // new RichText(
-                          //   text: new TextSpan(
-                          //     style: new TextStyle(
-                          //       fontSize: 14.0,
-                          //       color: Colors.black,
-                          //     ),
-                          //     children: <TextSpan>[
-                          //       new TextSpan(text: '${eventss.title} \n\n',style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 18.0)),
-                          //       new TextSpan(text: 'Events \t\t\t\t\t\t  ${eventss.title} \n\n'),
-                          //       new TextSpan(text: 'Date \t\t\t\t\t\t  ${eventss.dates} \n\n'),
-                          //       new TextSpan(text: 'Time \t\t\t\t\t\t  ${eventss.starttime} - ${eventss.endtime}  \n\n'),
-                          //       new TextSpan(text: 'Location \t\t\t\t\t\t  ${eventss.location} \n\n'),
-                          //       new TextSpan(text: 'Admission \t\t\t\t\t\t  ${eventss.fee} \n\n'),
-                          //       new TextSpan(text: 'Benefits \t\t\t\t\t\t  ${eventss.benifits} \n\n'),
-                          //     ],
-                          //   ),
-                          // ),
+                          Container(
+                            color: Colors.blue,
+                            child: TextButton(
+                                child: Text('Show on Calender',style: TextStyle(color: Colors.white),),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => CalenderView(),
+                                      ));
+                                }
+                            ),
+                          )
                         ],
                       ),
                     ),
