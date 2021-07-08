1c3cec737592f00d14e74a30f76340755fb329e2
Ammar Afzal <ammarafzal335@icloud.com>
2021-07-08 16:09:13 +0500

    a

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:nilesisters/API_Data/getHome.dart';
+import 'package:nilesisters/screens/SizeConfig.dart';
+import 'package:nilesisters/screens/pdf_api.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 class Home extends StatefulWidget {
   @override
@@ -9,14 +12,38 @@ class _HomeState extends State<Home> {
   final Completer <WebViewController> _controller = Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: ListView(
-           children: [
-             Image.asset('assets/images/nilesisters.png'),
-
-           ],
+        child: FutureBuilder(
+          future: fetchHome(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: 1,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, index) {
+                  GetHomePage hm = snapshot.data[index];
+                  return Column(
+                    children: [
+                      Image.network(
+                        ''+ hm.image,
+                        height: 300,
+                        width: SizeConfig.screenWidth * 1,
+                      ),
+                      Text(hm.news, style: TextStyle(fontSize: 16),)
+                    ],
+                  );
+                },
+              );
+            }
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: new AlwaysStoppedAnimation<Color>(Color.fromRGBO(216, 56, 48, 1)),
+              ),
+            );
+          },
         ),
       ),
       // body: WebView(
